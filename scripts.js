a4b6d744ad215aadf663e82f48a07caca53fe7ea
sakaz4 <88734252+sakaz4@users.noreply.github.com>
2021-08-21 11:16:01 +0300

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,89 @@
+let string1 = "привет",
+    num = 123,
+    numFloat = 15.8,
+    confirm = true,
+    string2 = "true";
+
+console.log(string1, typeof string);
+console.log(num, typeof num);
+console.log(numFloat, typeof numFloat);
+console.log(confirm, typeof confirm);
+console.log(string2, typeof string2);
+
+let a1 = 5 % 3;
+    a2 = 3 % 5;
+    a3 = 5 + '3';
+    a4 = '5' - 3;
+    a5 = 75 + 'кг';
+    a6 = 785 * 653;
+    a7 = 100 / 25;
+    a8 = 0 * 0;
+    a9 = 0 / 2;
+    a10 = 89 / 0;
+    a11 = 98 + 2;
+    a12 = 5 - 98;
+    a13 = (8 + 56 * 4) / 5;
+    a14 = (9 - 12) * 7 / (5 + 2);
+    a15 = +"123";
+    a16 = 1 || 0;
+    a17 = false || true;
+    a18 =true > 0;
+
+console.log(a1, typeof str);
+console.log(a2, typeof num);
+console.log(a3, typeof numFloat);
+console.log(a4, typeof num);
+console.log(a5, typeof num);
+console.log(a6, typeof num);
+console.log(a7, typeof num);
+console.log(a8, typeof num);
+console.log(a9, typeof num);
+console.log(a10, typeof num);
+console.log(a11, typeof num);
+console.log(a12, typeof num);
+console.log(a13, typeof num);
+console.log(a14, typeof num);
+console.log(a15, typeof num);
+console.log(a16, typeof num);
+console.log(a17, typeof num);
+console.log(a18, typeof num);
+
+
+let height = 23,
+    width = 10,
+    SPryam = height * width;
+console.log(SPryam);
+
+let height2 = 10,
+    d = a7,
+    π = 3.14,
+    so = π * (d / 2),
+    VCilindra = height2 * so;
+console.log(VCilindra);
+
+let rc = 5,
+    SKruga = π * rc;
+console.log(SKruga);
+
+let ot_a = 5,
+    ot_b = 7,
+    h = 10,
+    STrap = ot_a * ot_b * h;
+console.log(STrap);
+
+/*let credit = 2000000,
+    bid = 0.1,
+    payment = 60,
+    i = bid / 12,
+    ap*/
+
+let aa = 8,
+    b = 3,
+    x = ( b * 2 + 16 - aa) / 2;
+console.log(x);
+
+let xx = (15 * b - aa) / 3;
+console.log(xx);
+
+let xxx = 23780 / 14;
+console.log(xxx);
