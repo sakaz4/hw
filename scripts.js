5095981dce27fa60d457ec2bca00b6f30460ac44
sakaz4 <88734252+sakaz4@users.noreply.github.com>
2021-08-22 20:11:32 +0300

    hw1

--- scripts.js
+++ scripts.js
@@ -29,24 +29,24 @@ let a1 = 5 % 3;
     a17 = false || true;
     a18 =true > 0;
 
-console.log(a1, typeof str);
-console.log(a2, typeof num);
-console.log(a3, typeof numFloat);
-console.log(a4, typeof num);
-console.log(a5, typeof num);
-console.log(a6, typeof num);
-console.log(a7, typeof num);
-console.log(a8, typeof num);
-console.log(a9, typeof num);
-console.log(a10, typeof num);
-console.log(a11, typeof num);
-console.log(a12, typeof num);
-console.log(a13, typeof num);
-console.log(a14, typeof num);
-console.log(a15, typeof num);
-console.log(a16, typeof num);
-console.log(a17, typeof num);
-console.log(a18, typeof num);
+    console.log(a1, typeof a1);
+    console.log(a2, typeof a2);
+    console.log(a3, typeof a3);
+    console.log(a4, typeof a4);
+    console.log(a5, typeof a5);
+    console.log(a6, typeof a6);
+    console.log(a7, typeof a7);
+    console.log(a8, typeof a8);
+    console.log(a9, typeof a9);
+    console.log(a10, typeof a10);
+    console.log(a11, typeof a11);
+    console.log(a12, typeof a12);
+    console.log(a13, typeof a13);
+    console.log(a14, typeof a14);
+    console.log(a15, typeof a15);
+    console.log(a16, typeof a16);
+    console.log(a17, typeof a17);
+    console.log(a18, typeof a18);
 
 
 let height = 23,
@@ -71,11 +71,15 @@ let ot_a = 5,
     STrap = ot_a * ot_b * h;
 console.log(STrap);
 
-/*let credit = 2000000,
-    bid = 0.1,
-    payment = 60,
-    i = bid / 12,
-    ap*/
+let S = 2000000,
+    p = 10,
+    years = 5;
+    
+    let monPay = S * (((p / 100 / 12) * (1 + (p / 100 / 12)) ** (12 * years)) / ((1 + (p / 100 / 12)) ** (12 * years) - 1));
+
+    let Pereplata = (years * 12) * monPay - S;
+console.log('Pereplata = ' + Pereplata);
+
 
 let aa = 8,
     b = 3,
