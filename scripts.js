2391121be373917f2d78a665e40e1483a767a36a
sakaz4 <88734252+sakaz4@users.noreply.github.com>
2021-08-27 18:55:16 +0300

    js3

--- scripts.js
+++ scripts.js
@@ -1,94 +1,55 @@
-let string1 = "привет",
-    num = 123,
-    numFloat = 15.8,
-    confirm = true,
-    string2 = "true";
+console.log('Задание №1');
 
-console.log(string1, typeof string);
-console.log(num, typeof num);
-console.log(numFloat, typeof numFloat);
-console.log(confirm, typeof confirm);
-console.log(string2, typeof string2);
+for(let v = 1; v < 51; v++){
+    console.log(v);
+}
 
-let a1 = 5 % 3;
-    a2 = 3 % 5;
-    a3 = 5 + '3';
-    a4 = '5' - 3;
-    a5 = 75 + 'кг';
-    a6 = 785 * 653;
-    a7 = 100 / 25;
-    a8 = 0 * 0;
-    a9 = 0 / 2;
-    a10 = 89 / 0;
-    a11 = 98 + 2;
-    a12 = 5 - 98;
-    a13 = (8 + 56 * 4) / 5;
-    a14 = (9 - 12) * 7 / (5 + 2);
-    a15 = +"123";
-    a16 = 1 || 0;
-    a17 = false || true;
-    a18 =true > 0;
-
-    console.log(a1, typeof a1);
-    console.log(a2, typeof a2);
-    console.log(a3, typeof a3);
-    console.log(a4, typeof a4);
-    console.log(a5, typeof a5);
-    console.log(a6, typeof a6);
-    console.log(a7, typeof a7);
-    console.log(a8, typeof a8);
-    console.log(a9, typeof a9);
-    console.log(a10, typeof a10);
-    console.log(a11, typeof a11);
-    console.log(a12, typeof a12);
-    console.log(a13, typeof a13);
-    console.log(a14, typeof a14);
-    console.log(a15, typeof a15);
-    console.log(a16, typeof a16);
-    console.log(a17, typeof a17);
-    console.log(a18, typeof a18);
+for(let u = 35; u > 7; u--){
+    console.log(u);
+}
 
 
-let height = 23,
-    width = 10,
-    SPryam = height * width;
-console.log(SPryam);
 
-let height2 = 10,
-    d = a7,
-    π = 3.14,
-    so = π * (d / 2),
-    VCilindra = height2 * so;
-console.log(VCilindra);
+console.log('Задание №2');
 
-let rc = 5,
-    SKruga = π * rc;
-console.log(SKruga);
+let z = 89;
 
-let ot_a = 5,
-    ot_b = 7,
-    h = 10,
-    STrap = ot_a * ot_b * h;
-console.log(STrap);
+while(z >= 12) { 
+    console.log(z);
+    z--;
+}
+console.log(z);
 
-let S = 2000000,
-    p = 10,
-    years = 5;
-    
-    let monPay = S * (((p / 100 / 12) * (1 + (p / 100 / 12)) ** (12 * years)) / ((1 + (p / 100 / 12)) ** (12 * years) - 1));
+console.log('Задание №3');
 
-    let Pereplata = (years * 12) * monPay - S;
-console.log('Pereplata = ' + Pereplata);
+let q = 100; 
+let sum = 0;
+for (let i = 1; i <= q; i++) {
+    sum = 0;
+    for (let j = 1; j <= i; j++) {
+        sum += j;
+    }
+}
+console.log(' Sum ' + q + ' = ' + sum)
+
+console.log('Задание №4');
+
+let a = 5; 
+let sum2 = 0;
+for (let i = 1; i <= a; i++) {
+    sum = 0;
+    for (let j = 1; j <= i; j++) {
+        sum += j;
+    }
+    console.log(' Sum ' + i + ' = ' + sum)
+}
+
+console.log('Задание №5');
+
+for(let k = 8; k <= 56; k++) { 
+    if (k % 2 == 1) continue; // выводим только чётные числа
+    console.log(k);
+}
 
 
-let aa = 8,
-    b = 3,
-    x = ( b * 2 + 16 - aa) / 2;
-console.log(x);
-
-let xx = (15 * b - aa) / 3;
-console.log(xx);
-
-let xxx = 23780 / 14;
-console.log(xxx);
-
+console.log('Задание №6');
