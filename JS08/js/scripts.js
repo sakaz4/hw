23a68da2fb2c7e6eea0414faf779179937866fc1
sasha <88734252+sakaz4@users.noreply.github.com>
2021-09-14 14:45:58 +0300

    Update scripts.js

--- JS08/js/scripts.js
+++ JS08/js/scripts.js
@@ -13,6 +13,35 @@ console.log(metaUtf8);
 document.head.appendChild(title);
 document.head.appendChild(metaUtf8);
 
+let h1 = document.createElement('h1');
+h1.innerHTML = 'Choose Your Option';
+
+let p1 = document.createElement('p');
+p1.innerHTML = 'But I must explain to you how all this mistaken idea of denouncing';
+document.body.appendChild(h1);
+document.body.appendChild(p1);
+
+
+let div1 = document.createElement('div'); //добавляем div для левой части
+div1.classList.add('block1');
+document.body.appendChild(div1);
+
+let h4 = document.createElement('h4');
+h4.innerHTML = 'freelancer';
+div1.appendChild(h4);
+
+let h2 = document.createElement('h2');
+h2.innerHTML = 'Initially designed to ';
+div1.appendChild(h2);
+
+let p3 = document.createElement('p');
+p3.innerHTML = 'But I must explain to you how all this mistaken idea of denouncing ';
+div1.appendChild(p3);
+
+
+let button1 = document.createElement('button');
+button1.innerHTML = 'I am A Button!'; 
+div1.appendChild(button1);  // спросить про currentDiv
 
 
 
@@ -20,6 +49,11 @@ document.head.appendChild(metaUtf8);
 
 
 
+
+
+let div2 = document.createElement('div'); //добавляем div для левой части
+div2.classList.add('block2');
+document.body.appendChild(div2);
 
 
 
