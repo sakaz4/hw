66c754957dbf0ebfd4ca7f7d7224b582daee9ae1
sasha <88734252+sakaz4@users.noreply.github.com>
2021-09-22 14:48:52 +0300

    22.09 - 14:48

--- JS09/js/scripts.js
+++ JS09/js/scripts.js
@@ -1,10 +1,49 @@
-function want() {
-    let list = document.querySelector("#do");
-};
+let title = document.createElement('title');
+title.innerHTML = 'Want';  
 
-li.style.display = 'none';
-document.write.want;
-// вызвали функцию --> список с названием и скрытыми li
+let metaUtf8 = document.createElement('meta');
+metaUtf8.setAttribute('charset', 'UTF-8');
+
+console.log(title);
+console.log(metaUtf8);
+
+document.head.appendChild(title);
+document.head.appendChild(metaUtf8);
+
+
+
+
+let f = prompt ('Пишите сюда');
+    if ( f == 'want') {
+        let h1 = document.createElement('h1');
+        h1.innerHTML = 'list to do';
+        document.body.appendChild(h1);
+        h1.style.marginTop = '15px';
+        h1.style.marginBottom = '20px';
+        h1.style.marginLeft = '2px';
+        h1.style.marginRight = '2px';
+        h1.style.backgroundColor = 'blue';
+        h1.style.color = 'aliceblue';
+        h1.style.textAlign = 'center';
+
+        let div1 = document.createElement('div');
+        //div1.innerHTML = '1st div';
+        div1.classList.add('1st');
+        document.body.appendChild(div1);
+            
+        let ul1 = document.createElement('ul');
+        ul1.innerHTML = `<li><input placeholder="хотелку писать в сюда" type="text" size="30" ></li>
+        <li>.</li>
+        <li>.</li>
+        <li>.</li>
+        <li>.</li>
+        <li>.</li>
+        <li>.</li>`;
+        document.body.appendChild(ul1);
+
+} else {
+    alert('неправильно хочешь, пробуй ещё');
+}
 
 
 
