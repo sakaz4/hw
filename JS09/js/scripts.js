36c7201fda1cb6d791d0c2c774207ae175031425
sasha <88734252+sakaz4@users.noreply.github.com>
2021-09-26 20:28:17 +0300

    1

--- JS09/js/scripts.js
+++ JS09/js/scripts.js
@@ -26,24 +26,45 @@ let f = prompt ('Пишите сюда');
         let div1 = document.createElement('div');
         //div1.innerHTML = '1st div';
         div1.classList.add('1st');
-        document.body.appendChild(div1);
-            
+        document.body.appendChild(div1); 
         let ul1 = document.createElement('ul');
-        ul1.innerHTML = `<form><li><input class="input" placeholder="хотелку писать в сюда" type="text" size='30'></li>
-        <li><input type="button" value="отправить" onclick="alert('принято')"/></li></form>
-        <li>.</li>
-        <li>.</li>
-        <li>.</li>
-        <li>.</li>
-        <li>.</li>
-        <li>.</li>`;
+        ul1.innerHTML = `<form>
+        <li>
+            <input id="btn" type="button" value="жмякай" onclick="prompt('1-6')"/>
+        </li>
+        </form>
+        `;
         document.body.appendChild(ul1);
 } else {
     alert('неправильно хочешь, пробуй ещё');
 }
 
+let arr = (6);
+arr = document.querySelector('#btn');
+switch(arr) {
+case 1: arr.splice(0, 0, 'buy bike');
+break
+case 2: arr.splice(1, 0, 'do bike fit');
+break
+case 3: arr.splice(2, 0, 'set up the work of all components');
+break
+case 4: arr.splice(3, 0, 'set up tubeless tires');
+break
+case 5: arr.splice(4, 0, 'ride fast die young');
+break
+case 6: arr.splice(5, 0, 'be fast and happy');
+break
+};
+
+document.write(arr);
+
+
+
 /*const inputText = document.querySelector('.input');
 inputText.value*/
+
+
+//<li><input class="input" placeholder="1-6" type="text" value"1-6"></li>
 
 
 /*const inputText = document.querySelector('input')
