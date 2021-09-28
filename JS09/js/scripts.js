2354ed1516080947325c649646a688980e4160ef
sasha <88734252+sakaz4@users.noreply.github.com>
2021-09-28 14:54:52 +0300

    09

--- JS09/js/scripts.js
+++ JS09/js/scripts.js
@@ -8,11 +8,12 @@ console.log(metaUtf8);
 document.head.appendChild(title);
 document.head.appendChild(metaUtf8);
 
+
 window.onload = init(); // создаём todo на странице
 
 function init() {   
     let todoList = document.createElement("div"); // создаем div для нашего todo списка
-    todoList.innerHTML = '<ul class="todo"><li><input class="input" placeholder="пишите сюда" type="text"></li></ul>'; // ul и первый его элемент - поле ввода
+    todoList.innerHTML = '<ul class="todo"><li><h3>список задач</h3></li><li><input class="input" placeholder="пишите сюда" type="text" value"1-6"></li></ul>'; // ul и первый его элемент - поле ввода
     document.body.appendChild(todoList); // добавляем его в body страницы
     document.querySelector('.input').onchange = processInput; // привязываем к событию изменения инпута свою функцию обработчик
     // что-то написали и нажали enter - вызвалась функция processInput
@@ -36,3 +37,107 @@ function editListItem(event) {
     let newValue = prompt("Что хочешь поменять?"); // спрашиваем про новое значение
     event.target.innerText = newValue; // изменяем текст на тот, который ввели в prompt
 }
+
+
+div.style.marginTop = '130px';
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*let f = prompt ('Пишите сюда');
+    if ( f == 'want') {
+        let h1 = document.createElement('h1');
+        h1.innerHTML = 'list to do';
+        document.body.appendChild(h1);
+
+let div1 = document.createElement('div');
+        div1.classList.add('1st');
+        document.body.appendChild(div1); 
+        let ul1 = document.createElement('ul');
+        
+        let li = document.createElement('li');
+        let inputValue = document.getElementById('myInput').value;
+        let t = document.createTextNode(inputValue);
+        li.appendChild(t);
+        ul.appendChild(li);
+
+        let ul1 = document.createElement('ul');
+        ul1.innerHTML = `<form>
+        <li><input class="input" placeholder="1-6" type="text" value"1-6"></li>
+        </form>`;
+
+        document.body.appendChild(ul1);
+} else {
+        alert('неправильно хочешь, пробуй ещё');
+}
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
