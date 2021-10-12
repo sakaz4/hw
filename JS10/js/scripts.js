41264cc960af3dbe10e4bfd2b54bdb3766a66e21
sasha <88734252+sakaz4@users.noreply.github.com>
2021-10-12 14:04:08 +0300

    14:03

---
+++ JS10/js/scripts.js
@@ -0,0 +1,67 @@
+let object34 = {
+    name: 'sasha',
+    talk: 'i want to eat'
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*const D = function() {
+    this.create = function(name) {
+        const elem = document.createElement(name);
+        return elem;
+    }
+
+    this.attr = function(elem, name, value) {
+        elem.setAttribute(name, value);
+        return elem.getAttribute(name); //возвращнение значения поэтому return
+    }
+
+    this.html = function(elem, value) {
+        if (value) elem.innerHTML = value;
+        return elem.innerHTML;
+    }  
+};
+
+const d = new D();
+console.log(d);
+
+const h2 = d.create('h2');
+const p1 = d.create('p');
+const div = d.create('div');
+
+d.html(h2, 'element h2');
+d.html(p1, 'element p');
+d.html(div, 'element div');
+
+d.attr(h2, 'class', 'title');
+
+console.log(h2);
+console.log(p1);
+console.log(div);
+
+console.log(d.html(p1));
+console.log(d.attr(h2,'class'));*/
