b519f92d89771f96b6be688f57a40e22ad218b28
sasha <88734252+sakaz4@users.noreply.github.com>
2021-10-19 12:02:34 +0300

    12:02

--- JS11/js/scripts.js
+++ JS11/js/scripts.js
@@ -1,3 +1,41 @@
 class Contacts {
     
+};
+
+
+class User {
+    constructor(data) {
+        this.id = id.data;
+        this.name =
+        this.email =
+        this.address = 
+        this.phone = 
+    }
+    edit(obj) {
+
+
+    }
+
+    get() {
+        return {
+
+        }
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
