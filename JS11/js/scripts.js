0500c270c1ff2307ad1af15952747c1823b13bc1
sasha <88734252+sakaz4@users.noreply.github.com>
2021-10-19 13:06:57 +0300

    4 new contacts

--- JS11/js/scripts.js
+++ JS11/js/scripts.js
@@ -5,11 +5,11 @@ class Contacts {
 
 class User {
     constructor(data) {
-        this.id = id.data;
-        this.name =
-        this.email =
-        this.address = 
-        this.phone = 
+        this.id = data.id;
+        this.name = data.name;
+        this.email = data.email;
+        this.address = data.address;
+        this.phone = data.phone;
     }
     edit(obj) {
 
@@ -21,11 +21,23 @@ class User {
 
         }
     }
+
+    showName() {
+        console.log(this.name);
+    }
 }
 
+let contact1 = new User({ id: '12',  name: 'Vasya', email: 'v@gmail.com', address: 'm-city', phone:'with keys'});
+console.log(contact1);
 
+let contact2 = new User({ id: '23',  name: 'Petya', email: 'p@gmail.com', address: 'm-city', phone:'with keys and display'});
+console.log(contact2);
 
+let contact3 = new User({ id: '34',  name: 'Valodia', email: 'val@gmail.com', address: 'm-city', phone:'with keys, display and battery'});
+console.log(contact3);
 
+let contact4 = new User({ id: '45',  name: 'Batman', email: 'batman@gmail.com', address: 'gotham-city', phone:'batphone'});
+console.log(contact4);
 
 
 
